91720112f116a8e04755093c6a2564457c1acb27
Щегольков С.Д. <shchegolkov_sd@lgnch.spb.ru>
2026-01-28 14:11:39 +0300

    test2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "eslint/config";
 import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "module", ecmaVersion: "latest", globals:{...globals.browser} }, rules: { "no-unused-vars": "warn", "no-console": "warn" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module", ecmaVersion: "latest", globals:{...globals.browser} }, rules: { "no-unused-vars": "warn", "no-console": "error" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   prettierConfig,
 ]);
