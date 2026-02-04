722fa1632e79da648cbe47a25dac756d569c3b28
Щегольков С.Д. <shchegolkov_sd@lgnch.spb.ru>
2026-02-04 10:39:16 +0300

    weatherAppYandEmpty

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = "97d93f1704dcb8e35dd2045c8e75710d"; // Получите на openweathermap.org
+//export const BASE_URL = "https://api.openweathermap.org/data/2.5";
+export const BASE_URL = "https://ya.ru";
